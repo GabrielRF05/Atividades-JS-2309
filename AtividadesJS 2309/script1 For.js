f46a69775a87abd45b0f19e8acdfc3355bce6e91
GabrielRF05 <gabrieldosreis512@gmail.com>
2024-09-30 15:54:53 -0300

    Delete AtividadesJS 2309 directory

--- AtividadesJS 2309/script1 For.js
+++
@@ -1,8 +0,0 @@
-let num = parseFloat(prompt("Insira o número para tabuada: "));
-let result = 0;
-
-
-    for(let i=0; i<=10 ; i++){
-    result = num * i;
-    console.log("A tabuada é " + result);
-}
--- AtividadesJS 2309/script2 For.js
+++
@@ -1,8 +0,0 @@
-for(let num = 1; num <= 9; num++){
-
-    for(let i = 0; i<= 10; i++){
-        console.log(num*i)
-
-    }
-    console.log('')
-}

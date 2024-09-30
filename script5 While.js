9ebd57c58f82b42208f0c1d6fa0f21fe57aeb2c2
GabrielRF05 <gabrieldosreis512@gmail.com>
2024-09-30 15:55:11 -0300

    Add files via upload

--- script5 While.js
+++ script5 While.js
@@ -1,23 +1,34 @@
 let continuar = true
-let menorIdade = 0
+let menorIdade = 200
 let maiorIdade = 0
 let alturaMenorIdade = 0
 let pesoMaiorIdade = 0
+let somaIdades = 0;
+let quantidadePessoas = 0;
 
 while(continuar){
     let altura = parseFloat(prompt("Insira sua altura: "))
     let peso = parseFloat(prompt("Insira seu peso: "))
     let idade = parseFloat(prompt("Insira sua idade"))
 
+    somaIdades += idade;
+    quantidadePessoas++;
+
+    if(menorIdade > idade){
+        menorIdade=idade
+        alturaMenorIdade = altura
+    }
+
+    if(maiorIdade< idade){
+        maiorIdade = idade
+        pesoMaiorIdade = peso
+    }
+
     continuar = confirm("Deseja continuar?")
 
 }
-if(continuar=false){
-    menorIdade = Math.min(idade)
-    alert("A pessoa mais jovem tem " + menorIdade)
-}
+let mediaIdades = somaIdades / quantidadePessoas;
 
-
-
-
-
+alert(`A altura da pessoa mais jovem é ${alturaMenorIdade}`)
+alert(`O peso da pessoa mais velha é: ${pesoMaiorIdade}`)
+alert(`A média de idade das pessoas é: ${mediaIdades}`);
